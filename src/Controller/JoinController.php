9d3f37404abb93a75fd01f5ec80f9df0bc155eed
winter_night <bh.hl.c.108.tr@gmail.com>
2020-08-19 16:57:50 +0900

    Create MemoController

--- src/Controller/JoinController.php
+++ src/Controller/JoinController.php
@@ -4,13 +4,12 @@ namespace App\Controller;
 
 class JoinController extends AppController
 {
-    
-    
+
     public function initialize()
     {
         $this->name = 'Join';
         $this->viewBuilder()->autoLayout(false);
-        $this->viewBuilder()->layout('Join');
+        $this->autoRender = false;
 
     }
 
--- src/Template/Layout/join.ctp
+++ src/Template/Layout/join.ctp
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <?php
-        echo $this->Html->css('cake.hello');
+        echo $this->Html->css('cake.Join');
         echo $this->fetch('meta');
         echo $this->fetch('css');
     ?>
